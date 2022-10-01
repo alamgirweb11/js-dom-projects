fb631bf1dd581ac11d283837c47c03b3e99b491d
alamgirweb11 <alamgirweb11@gmail.com>
2022-10-01 21:59:40 +0600

    add form validation and store data in posts object and display post

--- simple-crud/main.js
+++ simple-crud/main.js
@@ -1,4 +1,39 @@
 let form = document.getElementById('form')
-form.addEventListener('submit', () => {
-       alert('Submitted!');
+let description = document.getElementById("description")
+let errorMsg = document.getElementById("errorMsg")
+let postAppend = document.getElementById("posts")
+
+form.addEventListener('submit', (event) => {
+       event.preventDefault()
+       formValidation()
 })
+
+let posts = {}
+
+function formValidation(){
+       if(description.value){
+              errorMsg.innerHTML = ""
+              acceptData()
+       }else{
+              errorMsg.innerHTML = "Post description is required."
+       }
+}
+
+
+let acceptData = () => {
+        posts['text'] = description.value
+        storePost()
+}
+
+let storePost = () => {
+       postAppend.innerHTML += `
+       <div>
+          <p>${posts.text}</p>
+          <span class="options">
+            <i class="fas fa-edit"></i>
+            <i class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+       `
+       description.value = ""
+}
