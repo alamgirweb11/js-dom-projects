69f3d85949fa6cf750ab0bda22e3c4d82e0fc62c
alamgirweb11 <alamgirweb11@gmail.com>
2022-10-23 23:09:38 +0600

    task store and show, and close modal after submitted form

--- todo-app/main.js
+++ todo-app/main.js
@@ -1,6 +1,11 @@
-let form = document.getElementById('form');
-let taskTitle = document.getElementById('textInput');
-let errorMsg = document.getElementById('msg');
+let form = document.getElementById('form'),
+ title = document.getElementById('textInput'),
+ date = document.getElementById('dateInput'),
+ description = document.getElementById('description'),
+ errorMsg = document.getElementById('msg'),
+ modalAddBtn = document.getElementById('modalAddBtn'),
+
+tasksWrapper = document.getElementById('tasks');
 
 form.addEventListener('submit', function(e){
      e.preventDefault();
@@ -8,10 +13,52 @@ form.addEventListener('submit', function(e){
 });
 
 let formValidation = () => {
-      if(taskTitle.value === ''){
+      if(title.value === ''){
          const errorText = 'Title is required!';
          errorMsg.innerHTML = errorText;
       }else{
-        errorMsg.innerHTML = ''
+        errorMsg.innerHTML = '';
+        inputValues();
+        modalAddBtn.setAttribute("data-bs-dismiss", "modal");
+        modalAddBtn.click();
+        /** 
+         * IIFE function =  immediately invoked functional expression
+         * syntax  (function funName(){})();
+        */
+       (() => {
+        modalAddBtn.setAttribute("data-bs-dismiss", "");
+       })();
       }
 }
+
+let data = [];
+
+let inputValues = () => {
+    data['title'] = title.value, 
+    data['date'] = date.value, 
+    data['description'] = description.value, 
+    createTask();
+}
+
+let createTask = () => {
+     tasksWrapper.innerHTML += `
+      <div>
+      <span>${data.title}</span>
+      <span>${data.date}</span>
+      <p>${data.description}</p>
+      <span>
+      <i class="fas fa-edit"></i>
+      <i class="fas fa-trash-alt"></i>
+      </span>
+      </div>
+     `;
+     resetForm();
+}
+
+function resetForm(){
+       title.value = "";
+       data.value = "";
+       description.value = "";
+}
+
+console.log(data);
