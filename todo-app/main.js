a3e5574a466a5c7333ffe1ae222856fa093dd4b9
alamgirweb11 <alamgirweb11@gmail.com>
2022-10-23 21:14:51 +0600

    form validation added

--- todo-app/main.js
+++ todo-app/main.js
@@ -1,6 +1,17 @@
 let form = document.getElementById('form');
+let taskTitle = document.getElementById('textInput');
+let errorMsg = document.getElementById('msg');
 
 form.addEventListener('submit', function(e){
      e.preventDefault();
-     alert('Submitted');
+     formValidation();
 });
+
+let formValidation = () => {
+      if(taskTitle.value === ''){
+         const errorText = 'Title is required!';
+         errorMsg.innerHTML = errorText;
+      }else{
+        errorMsg.innerHTML = ''
+      }
+}
